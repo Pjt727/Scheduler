ea32c53a048dff64233bbcf9fceee3a12520e1df
patrick <patrick@nytyler.com>
2024-08-23 13:21:44 -0400

    stractch

--- scratchpad/queries.sql
+++ scratchpad/queries.sql
@@ -1,13 +1,3 @@
-SELECT * FROM Subjects WHERE Subjects.school_code like 'CC';
-SELECT * FROM Subjects WHERE Subjects.code like 'CMPT';
-
-SELECT Sections.term_year, Sections.term_season, Count(*) FROM Sections
-GROUP BY Sections.term_season, Sections.term_year;
-
-
-SELECT * FROM Meetings WHERE Meetings.professor_id NOT NULL;
-SELECT * FROM Buildings;
-SELECT * FROM Rooms;
-
-DELETE FROM Terms;
-
+SELECT "Courses".code, "Courses".subject_code, "Courses".credits, "Courses".name, "Courses".description, "Courses".banner_id, "Courses".rowid
+FROM "Courses"
+WHERE "Courses".rowid IN (762)
